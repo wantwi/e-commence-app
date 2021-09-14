eea8ffc291efa8cdf87e25ae7424a728c6884787
William Antwi-Boasiako <william.antwi-boasiako@persol.net>
2021-09-14 16:56:32 +0000

    setting up userTypes varibles

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import React,{useState,useEffect} from 'react';
 import Homepage from './pages/HomePage/HomePage';
 import Shop from './pages/shop/Shop';
-import {Switch,Route} from "react-router-dom"
+import {Switch,Route,Redirect} from "react-router-dom"
 import Header from './components/header-component/Header';
 import Account from './pages/Accout/Account';
 import { auth,createUserProfile } from './firebase/firebase.util';
@@ -12,7 +12,7 @@ import { setcurrentUser } from './redux/user/user.action'
 
 
 function App(props) {
-const {setcurrentUser} = props
+const {setcurrentUser,currentUser} = props
    // const [currentUser, setcurrentUser] = useState(null)
 
     const getCurrentUser =  async ()=>{
@@ -54,7 +54,7 @@ const {setcurrentUser} = props
         
         <Route exact path="/" component={Homepage}/>
         <Route  path="/shop" component={Shop}/>
-        <Route  path="/signin" component={Account} />
+        <Route exact path="/signin" render={()=>currentUser? (<Redirect to='/' />) : <Account/>} />
       </Switch>
 
     
@@ -62,8 +62,14 @@ const {setcurrentUser} = props
   );
 }
 
+
+const mapStateToProps =({user}) =>({
+  currentUser: user.currentUser
+
+})
+
 const mapDispactToProps = dispatch =>({
   setcurrentUser: user => dispatch (setcurrentUser(user))
 })
 
-export default connect(null,mapDispactToProps)(App);
+export default connect(mapStateToProps,mapDispactToProps)(App);
