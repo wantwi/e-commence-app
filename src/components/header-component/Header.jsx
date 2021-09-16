e65101de76d0c6a39287ba356a8376ed98db1295
William Antwi-Boasiako <william.antwi-boasiako@persol.net>
2021-09-16 13:49:00 +0000

    remove logger in production

--- src/components/header-component/Header.jsx
+++ src/components/header-component/Header.jsx
@@ -16,7 +16,7 @@ function Header({ currentUser,hidden }) {
   console.log({currentUser});
   return (
     <div className="header">
-      <Link to="/" className="logo-container">WABeCOM
+      <Link to="/" className="logo-container">
         <img className="logo" src={Logo} alt="logo" />
       </Link>
       <div className="options">
